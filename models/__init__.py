8d9deab8443db2116613aac6cd4e5c75aa85ee82
Frainer Encarnacion <fraineralex2001@gmail.com>
2023-07-13 16:12:26 -0400

    [ADD] first commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
-
-from . import account_move_inherit
+from . import res_users
---
+++ models/res_users.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+class ResUsersInherit(models.Model):
+    _inherit = 'res.users'
+
+    def write(self, values):
+        # Verificar si el usuario logueado pertenece al grupo deseado
+        user = self.env.user
+        if not user.has_group('base.group_hr_employee'):
+        #if not user.has_group('account.group_account_invoice'):
+            raise UserError(_('No tiene permiso para editar este usuario, comuniquese con el administrador o con los encargados de recursos humanos.'))
+
+        res = super(ResUsersInherit, self).write(values)
+        return res
